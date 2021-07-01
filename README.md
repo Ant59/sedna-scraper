d30d270127b34a9e5afe50d04613c3ac83e0b65f
Antony Derham <antonyderham@gmail.com>
2021-07-01 10:51:09 +0100

    fix: Split CLI and logic

---
+++ README.md
@@ -0,0 +1,9 @@
+# Web Crawler
+
+A recursive web crawler to extract a map of pages and their links for a given domain.
+
+## Usage
+
+```sh
+./dist/app.js <domain>
+```
--- src/app.ts
+++ src/app.ts
@@ -1,62 +1,13 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import fetch from 'node-fetch';
-import parseUrl from 'parse-url';
-import isValidDomain from 'is-valid-domain';
-import { load } from 'cheerio';
-
-type ParsedUrl = ReturnType<typeof parseUrl>;
-type Sitemap = Record<string, string[]>;
-
-const unique = <T>(array: T[]): T[] => Array.from(new Set(array));
-
-export const extractLinks = async (url: ParsedUrl) => {
-  const response = await fetch(url.href);
-
-  const document = load(await response.text());
-  const anchors = document('a')
-    .map((_i, el) => document(el).attr('href'))
-    .get();
-
-  return unique(anchors)
-    .map((link) => parseUrl(link))
-    .filter((parsedLink) => parsedLink.resource === url.resource);
-};
-
-const buildMap: (sitemap: Sitemap, url: ParsedUrl) => Promise<Sitemap> = async (
-  sitemap,
-  url
-) => {
-  if (sitemap.hasOwnProperty(url.href)) return sitemap;
-
-  const links = await extractLinks(url);
-  sitemap[url.href] = links.map((link) => link.href);
-
-  return links.reduce(
-    async (sitemap, link) => await buildMap(await sitemap, link),
-    Promise.resolve(sitemap)
-  );
-};
-
-export const scrape = async (domain: string) => {
-  const domainUrl = parseUrl(domain, true);
-
-  if (!isValidDomain(domainUrl.resource)) {
-    console.error('Invalid domain name');
-    return;
-  }
-
-  if (domainUrl.pathname) {
-    console.error('Domain name should not include path');
-    return;
-  }
-
-  return await buildMap({}, domainUrl);
-};
+import { scrape } from './scrape';
 
 export const program = new Command();
 program
   .argument('<domain>', 'Domain name to scrape')
-  .action((domain) => console.log(scrape(domain)));
-// program.parse();
+  .action(async (domain) => {
+    const sitemap = await scrape(domain);
+    console.log(sitemap);
+  });
+program.parse();
